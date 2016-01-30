e12fdfb538862f03b28157cee282875b4b39d080
hhussain1629 <hhussain1629@gmail.com>
2016-01-30 18:25:19 -0500

    1-30-16

--- Hussain - Website/Models/IdentityModels.cs
+++ Hussain - Website/Models/IdentityModels.cs
@@ -32,9 +32,9 @@ namespace Hussain___Website.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+         : base("DefaultConnection", throwIfV1Schema: false)
         //public ApplicationDbContext()
-        //   : base("HussainsDB", throwIfV1Schema: false)
+        //: base("HussainsDB", throwIfV1Schema: false)
         {
         }
 
